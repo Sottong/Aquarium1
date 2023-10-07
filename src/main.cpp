762d51de5adbfe1eba78825c43037d81f4236363
Sottong <lutficartun@gmail.com>
2023-10-07 13:12:16 +0700

    Ultrasonic

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,62 @@
 #include <Arduino.h>
 
-#include <Wire.h>
-#include <TimeLib.h>
-#include <DS1307RTC.h>
+/*
+ * Ultrasonic Simple
+ * Prints the distance read by an ultrasonic sensor in
+ * centimeters. They are supported to four pins ultrasound
+ * sensors (liek HC-SC04) and three pins (like PING)))
+ * and Seeed Studio sensors).
+ *
+ * The circuit:
+ * * Module HR-SC04 (four pins) or PING))) (and other with
+ *   three pins), attached to digital pins as follows:
+ * ---------------------    --------------------
+ * | HC-SC04 | Arduino |    | 3 pins | Arduino |
+ * ---------------------    --------------------
+ * |   Vcc   |   5V    |    |   Vcc  |   5V    |
+ * |   Trig  |   12    | OR |   SIG  |   13    |
+ * |   Echo  |   13    |    |   Gnd  |   GND   |
+ * |   Gnd   |   GND   |    --------------------
+ * ---------------------
+ * Note: You do not obligatorily need to use the pins defined above
+ * 
+ * By default, the distance returned by the read()
+ * method is in centimeters. To get the distance in inches,
+ * pass INC as a parameter.
+ * Example: ultrasonic.read(INC)
+ *
+ * created 3 Apr 2014
+ * by Erick Simões (github: @ErickSimoes | twitter: @AloErickSimoes)
+ * modified 23 Jan 2017
+ * by Erick Simões (github: @ErickSimoes | twitter: @AloErickSimoes)
+ * modified 03 Mar 2017
+ * by Erick Simões (github: @ErickSimoes | twitter: @AloErickSimoes)
+ * modified 11 Jun 2018
+ * by Erick Simões (github: @ErickSimoes | twitter: @AloErickSimoes)
+ *
+ * This example code is released into the MIT License.
+ */
 
-void print2digits(int number) {
-  if (number >= 0 && number < 10) {
-    Serial.write('0');
-  }
-  Serial.print(number);
-}
+#include <Ultrasonic.h>
 
+/*
+ * Pass as a parameter the trigger and echo pin, respectively,
+ * or only the signal pin (for sensors 3 pins), like:
+ * Ultrasonic ultrasonic(13);
+ */
+Ultrasonic ultrasonic(5, 18);
+int distance;
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial) ; // wait for serial
-  delay(200);
-  Serial.println("DS1307RTC Read Test");
-  Serial.println("-------------------");
 }
 
 void loop() {
-  tmElements_t tm;
-
-  if (RTC.read(tm)) {
-    Serial.print("Ok, Time = ");
-    print2digits(tm.Hour);
-    Serial.write(':');
-    print2digits(tm.Minute);
-    Serial.write(':');
-    print2digits(tm.Second);
-    Serial.print(", Date (D/M/Y) = ");
-    Serial.print(tm.Day);
-    Serial.write('/');
-    Serial.print(tm.Month);
-    Serial.write('/');
-    Serial.print(tmYearToCalendar(tm.Year));
-    Serial.println();
-  } else {
-    if (RTC.chipPresent()) {
-      Serial.println("The DS1307 is stopped.  Please run the SetTime");
-      Serial.println("example to initialize the time and begin running.");
-      Serial.println();
-    } else {
-      Serial.println("DS1307 read error!  Please check the circuitry.");
-      Serial.println();
-    }
-    delay(9000);
-  }
+  // Pass INC as a parameter to get the distance in inches
+  distance = ultrasonic.read();
+  
+  Serial.print("Distance in CM: ");
+  Serial.println(distance);
   delay(1000);
 }
 
